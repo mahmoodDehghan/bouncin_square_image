a0ba1996ffbecd4af9bec8dcb19b36b362a31e1e
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2022-12-05 14:54:42 +0330

    +-parser

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,22 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
+int parsExp(String experession) {
+  final plusExp = RegExp(r'\+\d+');
+  final minusExp = RegExp(r'-\d+');
+  final normalNum = RegExp(r'\d+');
+  final pluses = plusExp.allMatches(experession);
+  final minuses = minusExp.allMatches(experession);
+  final first = int.tryParse(normalNum.firstMatch(experession)![0]!);
+  final plusSum = pluses
+      .map((e) => int.tryParse(e[0]!))
+      .reduce((value, element) => value! + element!);
+  final minusSum = minuses
+      .map((e) => int.tryParse(e[0]!))
+      .reduce((value, element) => value! + element!);
+  return plusSum! + first! + minusSum!;
+}
+
 int entryPars(String exp) {
   int sum = 0;
   final pluses = exp.split('+');
@@ -28,6 +44,7 @@ int entryPars(String exp) {
 void main() async {
   print(pow(2, 3));
   print(entryPars('1+2-3+5-45'));
+  print(parsExp('1+2+14-2-4'));
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
